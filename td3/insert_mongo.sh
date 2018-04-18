3a274702af2aa61ad2e049b8ae3432d988678d02
Franofarion <baptiste.domange@gmail.com>
2018-04-18 07:05:41 -0400

    td3 done

--- td3/insert_mongo.sh
+++ td3/insert_mongo.sh
@@ -1,7 +1,17 @@
 #! bin/bash
 
+echo "$(tput bold ; tput setaf 6)######################################"
+echo "$(tput bold ; tput setaf 2) Baptiste Domange - TD3 mongodb script $(tput bold ; tput setaf 6)"
+echo "$(tput bold ; tput setaf 6)######################################$(tput sgr0)"
+
+echo ""
+
+echo "$(tput setaf 1)Vous trouverez le résultat des requêtes dans le fichier $(tput bold) $PWD/td3_insert_mongo.log  $(tput sgr0)"
+
+echo ""
 
 #Exercice 1 : IMPORT
+echo "$(tput bold ; tput setaf 6)Exercice 1 : IMPORT  $(tput sgr0)"
 mongoimport --db 'stores_demo_td3' --collection customers --drop --file $PWD/c.unl
 mongoimport --db 'stores_demo_td3' --collection orders --drop --file $PWD/o.unl
 mongoimport --db 'stores_demo_td3' --collection cust_calls --drop --file $PWD/a.unl
@@ -9,6 +19,7 @@ mongoimport --db 'stores_demo_td3' --collection cust_calls --drop --file $PWD/a.
 echo "" > $PWD/td3_insert_mongo.log
 
 #Exercice 2 : FIND
+echo "$(tput bold ; tput setaf 6)Exercice 2 : FIND  $(tput sgr0)"
 #a) customer : dont customer_num > 101 (1pt)
 mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('customers').find({'customer_num' : { \$gt: 101 }})" >> $PWD/td3_insert_mongo.log
 
@@ -22,6 +33,7 @@ mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('cust_calls').fin
 mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('orders').find({}).sort({'order_date' : 1}).limit(1)" >> $PWD/td3_insert_mongo.log
 
 #Exercice 3 : INDEX 
+echo "$(tput bold ; tput setaf 6)Exercice 3 : INDEX $(tput sgr0)"
 #a. Unique sur la colonne : customer_num de la collection customer (1point)
 mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('customers').createIndex( { customer_num: 1 }, {unique : true})"
 
@@ -32,16 +44,19 @@ mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('cust_calls').cre
 mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('customers').find({'customer_num' : { \$gt: 101 }}).explain()" >> $PWD/td3_insert_mongo.log
 mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('cust_calls').find({'user_id' : /.*j/}).explain()" >> $PWD/td3_insert_mongo.log
 
-#Exercice 4 : Delete 
+#Exercice 4 : SQL to Mongo
+echo "$(tput bold ; tput setaf 6)Exercice 4 : SQL to Mongo $(tput sgr0)"
 #a. DELETE FROM CUST_CALLS WHERE customer_num = 121 ; 
-mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('cust_calls').remove({'customer_num' : 121})"
+mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('cust_calls').remove({'customer_num' : 121})" >> $PWD/td3_insert_mongo.log
 
 #SELECT COUNT(*) FROM CUST_CALLS (2points)
-mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('cust_calls').find().count()"
+mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('cust_calls').find().count()" >> $PWD/td3_insert_mongo.log
 
 
 #b. UPDATE FROM CUSTOMER SET FNAME = « Laurent » AND LNAME = « Revel » WHERE CUSTOMER_NUM = 101 (3pts)
+mongo localhost:27017/stores_demo_td3 --eval "db.getCollection('customers').update({ customer_num: 101}, {\$set :{ fname: 'Laurent', lname: 'Revel' }})"
 
+echo "$(tput setaf 1)Fin du script, vous pourrez voir les résultats des requêtes dans le fichier $(tput bold) $PWD/td3_insert_mongo.log $(tput sgr0)"
 
 
 exit
