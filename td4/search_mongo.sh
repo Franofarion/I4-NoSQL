d2cafd0e33a1ea52a4487fd8018f684d2fe7623e
Franofarion <baptiste.domange@gmail.com>
2018-05-22 15:27:53 +0200

    c'est beau

--- td4/search_mongo.sh
+++ td4/search_mongo.sh
@@ -21,32 +21,32 @@ echo "" > $PWD/td4_search_mongo.log
 #Exercice 1 : FIND
 echo "$(tput bold ; tput setaf 6) Exercice 1 $(tput sgr0)"  
 echo "$(tput bold ; tput setaf 6) Exercice 1 $(tput sgr0)" >> $PWD/td4_search_mongo.log
-#a. select * from customer where company MATCHES « *Sports* » and NOT MATCHES « *town* »"
-echo "$(tput bold ; tput setaf 6) a. $(tput sgr0)" >> $PWD/td4_search_mongo.log
+#a. select * from customer where company MATCHES « *Sports* » and NOT MATCHES « *town* »
+echo "$(tput bold ; tput setaf 6) a. select * from customer where company MATCHES « *Sports* » and NOT MATCHES « *town* » $(tput sgr0)" >> $PWD/td4_search_mongo.log
 mongo localhost:27017/stores_demo_td4 --eval "db.getCollection('customers').createIndex({ company : 'text'})" >> $PWD/td4_search_mongo.log
 mongo localhost:27017/stores_demo_td4 --eval 'db.getCollection("customers").find( { $text: { $search: "Sports -town" } } )' >> $PWD/td4_search_mongo.log
 #b. select * from cust_calls where call_descr MATCHES « *hero watch* » or MATCHES « *tennis* »
-echo "$(tput bold ; tput setaf 6) b. $(tput sgr0)" >> $PWD/td4_search_mongo.log
+echo "$(tput bold ; tput setaf 6) b. select * from cust_calls where call_descr MATCHES « *hero watch* » or MATCHES « *tennis* » $(tput sgr0)" >> $PWD/td4_search_mongo.log
 mongo localhost:27017/stores_demo_td4 --eval 'db.getCollection("cust_calls").find( { call_descr: { $regex : "tennis | Hero watch" } })'  >> $PWD/td4_search_mongo.log
 
 #Exercice 2 : FIND
 echo "$(tput bold ; tput setaf 6) Exercice 2 $(tput sgr0)"
 echo "$(tput bold ; tput setaf 6) Exercice 2 $(tput sgr0)" >> $PWD/td4_search_mongo.log
 #a. db.customer.find ( (address2 = nul ) )
-echo "$(tput bold ; tput setaf 6) a. $(tput sgr0)" >> $PWD/td4_search_mongo.log
+echo "$(tput bold ; tput setaf 6) a. db.customer.find ( (address2 = nul ) ) $(tput sgr0)" >> $PWD/td4_search_mongo.log
 mongo localhost:27017/stores_demo_td4 --eval "db.getCollection('customers').find ( { address2 : null } )" >> $PWD/td4_search_mongo.log
 #b. db.orders.find (( pai_date: ( $type = 11 )))
-echo "$(tput bold ; tput setaf 6) b. $(tput sgr0)" >> $PWD/td4_search_mongo.log
+echo "$(tput bold ; tput setaf 6) b. db.orders.find (( pai_date: ( $type = 11 ))) $(tput sgr0)" >> $PWD/td4_search_mongo.log
 mongo localhost:27017/stores_demo_td4 --eval 'db.getCollection("cust_calls").find( { paid_date: { $type : 10 } })' >> $PWD/td4_search_mongo.log
 
 #Exercice 3 : FIND ( return only s _id , fname et lname)
 echo "$(tput bold ; tput setaf 6) Exercice 3 $(tput sgr0)"
 echo "$(tput bold ; tput setaf 6) Exercice 3 $(tput sgr0)" >> $PWD/td4_search_mongo.log
 #a. db.customer.find ( ( state :’CA’, fname:’Arnold’))
-echo "$(tput bold ; tput setaf 6) a. $(tput sgr0)" >> $PWD/td4_search_mongo.log
+echo "$(tput bold ; tput setaf 6) a. db.customer.find ( ( state :’CA’, fname:’Arnold’)) $(tput sgr0)" >> $PWD/td4_search_mongo.log
 mongo localhost:27017/stores_demo_td4 --eval "db.getCollection('customers').find({ 'state' :'CA', 'fname':'Arnold'}, {'fname':1,'lname':1})" >> $PWD/td4_search_mongo.log
 #b. db.customer.find ( ( zipcod :’85008’))
-echo "$(tput bold ; tput setaf 6) b. $(tput sgr0)" >> $PWD/td4_search_mongo.log
+echo "$(tput bold ; tput setaf 6) b. db.customer.find ( ( zipcod :’85008’)) $(tput sgr0)" >> $PWD/td4_search_mongo.log
 mongo localhost:27017/stores_demo_td4 --eval "db.getCollection('customers').find({ 'zipcode' : '85008'}, {'fname':1,'lname':1})" >> $PWD/td4_search_mongo.log
 
 
